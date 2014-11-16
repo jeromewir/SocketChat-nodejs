b34e636b0073ce068e6a72c57237d84e15be563e
Jérôme Wirsztel <jeromwir@gmail.com>
2014-11-16 14:33:51 -0800

    Server forwarding messages

---
+++ app.js
@@ -0,0 +1,22 @@
+var app = require('express')();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+var port = 1337;
+
+app.get('/', function(req, res) {
+  res.send('Server is running')
+});
+
+io.on('connection', function(socket) {
+  socket.on('disconnect', function(socket) {
+
+  });
+
+  socket.on('userMsg', function(msg) {
+    io.emit('serverMsg', msg);
+  });
+});
+
+http.listen(port, function() {
+  console.log('Server launched on port ' + port);
+});
